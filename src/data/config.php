437329cf649f7184100bf35c20ebf55f736a6c07
Freddy Manston <freddymanston@gmail.com>
2020-11-16 01:38:35 +0200

    Update config.php
    Testing out connection to Heroku postgres db

--- src/data/config.php
+++ src/data/config.php
@@ -1,9 +1,9 @@
 <?php
 
 /** 
-  * Configuration for database connection.
+  * Configuration for local database connection.
   */
-
+/**
 $host			= "localhost";
 $username		= "phpuser";
 $password		= "m0s@1c$$";
@@ -12,3 +12,18 @@ $dsn			= "mysql:host=$host;dbname=$dbname";	// will use later
 $options		= array(
 					PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 				);
+*/
+
+/** 
+  * Configuration for Heroku database connection.
+  */
+
+$dsn = "pgsql:"
+    . "host=ec2-52-5-176-53.compute-1.amazonaws.com;"
+    . "dbname=daiqsc8i889tmh;"
+    . "user=uecwjhvfhzqkrf;"
+    . "port=5432;"
+    . "sslmode=require;"
+    . "password=b3fbbc28424ae6331197dc317a0732308efcc2d51f4ddd276fc25699d549acb6";
+
+$db = new PDO($dsn);
