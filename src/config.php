ccd1e4067d5700ece754ef4be59348e7c9f1ee7d
Freddy Manston <freddymanston@gmail.com>
2020-11-16 02:31:22 +0200

    Update config.php

--- src/config.php
+++ src/config.php
@@ -12,6 +12,7 @@ $dsn			= "mysql:host=$host;dbname=$dbname";	// will use later
 $options		= array(
 					PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 				);
+$db = new PDO($dsn, $username, $password, $options);
 */
 
 /** 
